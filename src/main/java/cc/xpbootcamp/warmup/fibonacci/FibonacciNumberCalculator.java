2641c16eff9c1d37f8c36c67c535631046494f9d
wjyan <wjyan@thoughtworks.com>
2020-02-15 20:24:15 +0800

    optimize performance

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciNumberCalculator.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciNumberCalculator.java
@@ -1,17 +1,40 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
+import java.util.Arrays;
+
 public class FibonacciNumberCalculator {
 
-    public int calculate(int step) {
+    private long[] resultCache;
+
+    int maxLength = 51;
+
+    public FibonacciNumberCalculator() {
+        this.resultCache = new long[maxLength];
+        Arrays.fill(resultCache, 1, this.resultCache.length, -1);
+        initCalculateResultCache();
+    }
+
+    private void initCalculateResultCache() {
+        this.resultCache[0] = 0;
+        this.resultCache[1] = 1;
+        for (int i = 0; i < maxLength; i++) {
+            getResultFromCache(i);
+        }
+    }
+
+    public long calculate(int step) {
         if (step < 0) {
             throw new IllegalArgumentException();
         }
-        if (step == 0) {
-            return 0;
+        return getResultFromCache(step);
+    }
+
+    private long getResultFromCache(int step) {
+        if (resultCache[step] >= 0) {
+            return resultCache[step];
         }
-        if (step == 1) {
-            return 1;
-        }
-        return 1;
+        long result = getResultFromCache(step - 2) + getResultFromCache(step - 1);
+        resultCache[step] = result;
+        return result;
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciNumberCalculatorTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciNumberCalculatorTest.java
@@ -11,17 +11,27 @@ class FibonacciNumberCalculatorTest {
 
     @Test
     void should_return_1_given_step_is_1() {
-        assertEquals(1, calculator.calculate(1));
+        assertEquals(1L, calculator.calculate(1));
     }
 
     @Test
     void should_return_0_given_step_is_0() {
-        assertEquals(0, calculator.calculate(0));
+        assertEquals(0L, calculator.calculate(0));
     }
 
     @Test
     void should_return_1_given_step_is_2() {
-        assertEquals(1, calculator.calculate(2));
+        assertEquals(1L, calculator.calculate(2));
+    }
+
+    @Test
+    void should_return_55_given_step_is_10() {
+        assertEquals(55L, calculator.calculate(10));
+    }
+
+    @Test
+    void should_return_55_given_step_is_50() {
+        assertEquals(12586269025L, calculator.calculate(50));
     }
 
     @Test
